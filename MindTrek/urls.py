fc3d6116fb67c80df5c78795a550885f926fd2b4
krmohammed <mohrahman558@gmail.com>
2024-12-28 11:50:43 +0000

    style: add custom styling

--- MindTrek/urls.py
+++ MindTrek/urls.py
@@ -21,5 +21,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-    path("auth/", include("auth.urls"))
+    path("auth/", include("users.urls"))
 ]
